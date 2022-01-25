3b1e5ac3017758a5335ae6f261460f5d8f83f3c2
Igor Buzykin <lexemz@gmail.com>
2022-01-26 00:17:30 +0300

    Вариант с категориями

--- ProgrammaticallyStaticTableView/TableViewController.swift
+++ ProgrammaticallyStaticTableView/TableViewController.swift
@@ -7,22 +7,35 @@
 
 import UIKit
 
-enum TableViewControllerCells: String, CaseIterable {
-    case today = "Сегодня"
-    case week = "Неделя"
-    case archive = "Архив"
+enum TableViewControllerConfiguration: String, CaseIterable {
+    case first = "Категория 1"
+    case second = "Категория 2"
+    case third = "Категория 3"
     
-    var icon: String {
-        switch self {
-            
-        case .today: return "clock"
-        case .week: return "calendar"
-        case .archive: return "archivebox"
-        }
+    static let cells = [
+        Self.first: [
+            CellForTableView(name: "Ячейка 1", iconName: "alarm"),
+            CellForTableView(name: "Ячейка 2", iconName: "keyboard")
+        ],
+        Self.second: [
+            CellForTableView(name: "Ячейка 1", iconName: "clock")
+        ]
+    ]
+}
+
+struct CellForTableView {
+    let name: String
+    let icon: UIImage
+    
+    init(name: String, iconName: String) {
+        self.name = name
+        self.icon = UIImage(systemName: iconName) ?? UIImage()
     }
 }
 
 class TableViewController: UITableViewController {
+    
+    private let cells = TableViewControllerConfiguration.cells
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -31,21 +44,30 @@ class TableViewController: UITableViewController {
     }
 
     // MARK: - Table view data source
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        TableViewControllerConfiguration.allCases.count
+    }
 
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        TableViewControllerConfiguration.allCases[section].rawValue
+    }
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        TableViewControllerCells.allCases.count
+        let key = TableViewControllerConfiguration.allCases[section]
+        guard let values = cells[key] else { return 0 }
+        return values.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell(style: .default, reuseIdentifier: "cell")
-        let currentCellContent = TableViewControllerCells.allCases[indexPath.row]
+        let key = TableViewControllerConfiguration.allCases[indexPath.section]
+        let value = cells[key]?[indexPath.row]
+        
         var content = cell.defaultContentConfiguration()
-        content.text = currentCellContent.rawValue
-        content.image = UIImage(systemName: currentCellContent.icon)
+        content.text = value?.name
+        content.image = value?.icon
         
         cell.contentConfiguration = content
-        cell.accessoryType = .disclosureIndicator // Arrows
         return cell
     }
-
 }
